2edd1e9cfc1c1e4b987e68a877da5cf1c9b97801
debi ayu <ayudebi52@gmail.com>
2022-11-12 13:35:28 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Nama                    : Deby Ayu Putri B")
+print("Jurusan                 : Sistem Informasi")
+print("Universitas             : President University")
+print("Alasan ingin masuk GDSC : saya ingin mengasah skill tentang computer science bersama GDSC")
